cff84c9cbc2ff788d995d917f544c4e599693935
Bonamente <56506035@protonmail.com>
2021-06-21 16:21:11 +0500

    feat: Add ShopPage component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import HomePage from './pages/homepage/HomePage';
 
-const HatsPage = () => <div><h1>Hats Page</h1></div>;
+import HomePage from './pages/homepage/HomePage';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/ShopPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/CollectionPreview';
+
+export default class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+
+    return (
+      <div className="shop-page">
+        {
+          collections.map(({
+            id,
+            title,
+            routeName,
+            items,
+          }) => <CollectionPreview key={id} title={title} routeName={routeName} items={items} />)
+        }
+      </div>
+    );
+  }
+}
