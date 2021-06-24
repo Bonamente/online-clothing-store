3a9355234d488e37b19398e5f0b4008b3fcdc602
Bonamente <56506035@protonmail.com>
2021-06-24 13:05:40 +0500

    feat: Add CustomButton component

---
+++ src/components/custom-button/CustomButton.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/button-has-type */
+import React from 'react';
+
+import './CustomButton.scss';
+
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+  <button
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
+
+export default CustomButton;
