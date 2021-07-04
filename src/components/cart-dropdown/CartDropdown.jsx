5a80c3db5c9f615d6e5efb01ec0ba42c72187c37
Bonamente <56506035@protonmail.com>
2021-07-04 11:54:54 +0500

    feat: Add memoized selectors with Reselect

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/CustomButton';
 import CartItem from '../cart-item/CartItem';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './CartDropdown.styles.scss';
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
