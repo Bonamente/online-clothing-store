c5df96a9d2cdcfcb8150cd632f3b2716e3d882a3
Bonamente <56506035@protonmail.com>
2021-07-05 17:02:51 +0500

    feat: Add addItem and removeItem functionality to CheckoutItem component

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,14 +1,19 @@
+/* eslint-disable object-curly-newline */
 import React from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../redux/cart/cart.actions';
 
 import './CheckoutItem.styles.scss';
 
-const CheckoutItem = ({ cartItem }) => {
-  const {
-    name,
-    imageUrl,
-    price,
-    quantity,
-  } = cartItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(actions.addItem(item)),
+  removeItem: (item) => dispatch(actions.removeItem(item)),
+  clearItem: (item) => dispatch(actions.clearItemFromCart(item)),
+});
+
+const CheckoutItem = ({ cartItem, addItem, removeItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
 
   return (
     <div className="checkout-item">
@@ -16,14 +21,24 @@ const CheckoutItem = ({ cartItem }) => {
         <img src={imageUrl} alt={name} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <div className="quantity">
+        <button type="button" className="decrease-button" onClick={() => removeItem(cartItem)}>
+          <span className="sr-only">decrease the quantity of this product in your cart</span>
+          &#10094;
+        </button>
+        <span className="value">{quantity}</span>
+        <button type="button" className="increase-button" onClick={() => addItem(cartItem)}>
+          <span className="sr-only">increase the quantity of this product in your cart</span>
+          &#10095;
+        </button>
+      </div>
       <span className="price">{price}</span>
-      <button type="button" className="remove-button">
-        <span className="sr-only">remove this item from the cart</span>
+      <button type="button" className="remove-button" onClick={() => clearItem(cartItem)}>
+        <span className="sr-only">remove this product from your cart</span>
         &#10005;
       </button>
     </div>
   );
 };
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
