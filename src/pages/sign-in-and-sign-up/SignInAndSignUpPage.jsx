047b90fb758e6c668f0291a7409a685c32123e3f
Bonamente <56506035@protonmail.com>
2021-07-15 17:57:56 +0500

    feat: Convert SignInAndSignUpPage to styled-component

--- src/pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import SignIn from '../../components/sign-in/SignIn';
 import SignUp from '../../components/sign-up/SignUp';
 
-import './SignInAndSignUpPage.styles.scss';
+import SignInAndSignUpContainer from './SignInAndSignUpPage.styles';
 
 const SignInAndSignUpPage = () => (
-  <div className="sign-in-and-sign-up">
+  <SignInAndSignUpContainer>
     <SignIn />
     <SignUp />
-  </div>
+  </SignInAndSignUpContainer>
 );
 
 export default SignInAndSignUpPage;
---
+++ src/pages/sign-in-and-sign-up/SignInAndSignUpPage.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SignInAndSignUpContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 850px;
+  margin: 30px auto;
+`;
+export default SignInAndSignUpContainer;
