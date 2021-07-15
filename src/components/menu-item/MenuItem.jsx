6db50ece4c3c1d689447990cd99133ed27a343e9
Bonamente <56506035@protonmail.com>
2021-07-15 16:10:54 +0500

    feat: Convert MenuItem to styled-component

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import './MenuItem.styles.scss';
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentTitle,
+  ContentSubtitle,
+} from './MenuItem.styles';
 
 const MenuItem = ({
   title,
@@ -14,13 +20,13 @@ const MenuItem = ({
   const handleClick = () => history.push(`${match.url}${linkUrl}`);
 
   return (
-    <div className={`${size} menu-item`} onClick={handleClick}>
-      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
-    </div>
+    <MenuItemContainer size={size} onClick={handleClick}>
+      <BackgroundImageContainer className="background-image" imageUrl={imageUrl} />
+      <ContentContainer>
+        <ContentTitle>{title.toUpperCase()}</ContentTitle>
+        <ContentSubtitle>SHOP NOW</ContentSubtitle>
+      </ContentContainer>
+    </MenuItemContainer>
   );
 };
 
